f822f3a5eb8a6afbcff95eba7c270f193499c1c7
Moscofian <137722753+Moscofian@users.noreply.github.com>
2024-08-16 12:55:06 -0300

    Opção de remover nome Parte 2

--- Script/js.js
+++ Script/js.js
@@ -1,3 +1,4 @@
+const inputBox = document.getElementById('nome');
  // Array para armazenar os nomes
  let nomes = [];
 
@@ -11,7 +12,16 @@
      } else {
          alert('Por favor, insira um nome.');
      }
+     console.log(nomes)
  }
+
+ inputBox.addEventListener('keydown', function(event) {
+    // Verifique se a tecla pressionada é Enter (código 13)
+    if (event.key === 'Enter') {
+        // Execute a função desejada
+        adicionarNome();
+    }
+});
 
  // Função para atualizar a exibição da lista de nomes
  function atualizarLista() {
@@ -26,10 +36,14 @@
 
  // Função para escolher um nome aleatoriamente
  function escolherNome() {
-     if (nomes.length === 0) {
+    if (nomes.length === 0) {
          alert('Por favor, adicione pelo menos um nome.');
-     } else {
+    } else {
          let indiceAleatorio = Math.floor(Math.random() * nomes.length);
          document.getElementById('resultado').innerText = "Nome escolhido: " + nomes[indiceAleatorio];
-     }
+        if(flexCheckDefault.checked == true){
+            nomes.splice(indiceAleatorio, 1)
+        }
+    }
+    console.log(nomes)
  }
